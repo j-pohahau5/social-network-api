4385375477cd6d13820af46aa4f95918f9f7c0dc
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-26 22:50:36 -0800

    added user get all users/user, create user, delete user, adn add/remove thought

---
+++ controllers/userController.js
@@ -0,0 +1,127 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+// Aggregate function to get the number of students overall
+const friendCount = async () =>
+  User.aggregate()
+    .count('friendCount')
+    .then((numberOfFriends) => numberOfFriends);
+
+// probably need the grade function to be a thought or reaction function most likely will just delete
+// 
+// Aggregate function for getting the overall grade using $avg
+// const grade = async (userId) =>
+//   Student.aggregate([
+//     // only include the given student by using $match
+//     { $match: { _id: ObjectId(studentId) } },
+//     {
+//       $unwind: '$assignments',
+//     },
+//     {
+//       $group: {
+//         _id: ObjectId(studentId),
+//         overallGrade: { $avg: '$assignments.score' },
+//       },
+//     },
+//   ]);
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const studentObj = {
+          users,
+          friendCount: await friendCount(),
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({
+            user,
+            // need to check if i switch grade to thoughts
+            //   grade: await grade(req.params.userId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a user and remove them from the thought
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No such user exists' })
+          : Thought.findOneAndUpdate(
+              { users: req.params.userId },
+              { $pull: { users: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: 'User deleted, but no thought found',
+            })
+          : res.json({ message: 'User successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add an thought to a user
+  addThought(req, res) {
+    console.log('You are adding an thought');
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { thoughts: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Remove thought from a user
+  removeThought(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { thought: { thoughtId: req.params.thoughtId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
---
+++ utils/data.js
@@ -0,0 +1,38 @@
+users = [
+  {
+    'username': 'jpohahau',
+    'email': 'jpohahau@hotmail.com',
+    'thoughts': [],
+    'friends': [],
+  },
+  {
+    'username': 'amiko',
+    'email': 'amiko@gmail.com',
+    'thoughts': [],
+    'friends': [],
+  },
+  {
+    'username': 'lundy',
+    'email': 'chrislundy@gmail.com',
+    'thoughts': [],
+    'friends': [],
+  },
+  {
+    'username': 'baku',
+    'email': 'baku@gmail.com',
+    'thoughts': [],
+    'friends': [],
+  },
+  {
+    'username': 'shiki',
+    'email': 'shiki@gmail.com',
+    'thoughts': [],
+    'friends': [],
+  },
+  {
+    'username': 'tony',
+    'email': 'tony@gmail.com',
+    'thoughts': [],
+    'friends': [],
+  },
+];
