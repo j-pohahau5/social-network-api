c08b4bc489ff2df0daab9ebb22ee4589e7667ad7
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-27 23:07:34 -0800

    added correct createThought

--- controllers/userController.js
+++ controllers/userController.js
@@ -135,7 +135,7 @@ module.exports = {
   removeFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $pull: { friend: { friendId: req.params.friendId } } },
+      { $pull: { friend: { _id: req.params.friendId } } },
       { runValidators: true, new: true }
     )
       .then((user) =>
