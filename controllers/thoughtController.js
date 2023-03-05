1e6dbefb8cc9fc84e3a12ae75908b0a3e1e8130f
Jonathan Pohahau <j.pohahau@outlook.com>
2023-03-04 23:56:56 -0800

    README

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -100,9 +100,9 @@ module.exports = {
   },
   // deleting a reaction
   deleteReaction(req, res) {
-    Thought.updateOne(
+    Thought.findOneAndUpdate(
       { id: req.params.thoughtId },
-      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { $pull: { reactions: { _id: req.params.reactionId } } },
     //   { runValidators: true, new: true }
     )
       .then((thought) =>
